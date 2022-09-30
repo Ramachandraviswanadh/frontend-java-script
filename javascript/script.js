a6531a87d5bf40f3270ef8b335585734795c03e5
Rama Chandra Viswanadh <ramachandraviswanadh333@gmail.com>
2022-09-30 15:42:12 +0530

    uploading the files

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// var age =15;
+// if (age < 3) {
+//     message = 'Hi, baby!';
+//   } else if (age < 18) {
+//     message = 'Hello!';
+//   } else if (age < 100) {
+//     message = 'Greetings!';
+//   } else {
+//     message = 'What an unusual age!';
+//   }
+//   console.log(message);
+
+//   let x
+//   console.log(x)
+
